a47f7e192f56591fb9b4b6e062037129aba8a437
pavan <guntreddipavankumar07@gmail.com>
2025-06-21 20:01:54 +0530

    new file three

--- pramod-portifolio/src/components/hero/Hero.jsx
+++ pramod-portifolio/src/components/hero/Hero.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect } from 'react';
 import './Hero.css';
 // import 'aos/dist/aos.css';
-import AOS from 'aos';
+// import AOS from 'aos';
 
 // import resumePDF from '../assets/resume.pdf'; // Ensure resume is placed in assets
 
 const Hero = () => {
   useEffect(() => {
-  AOS.init({ duration: 1000 });
+  // AOS.init({ duration: 1000 });
 }, []);
 
 
